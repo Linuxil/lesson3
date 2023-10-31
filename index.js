541c050bf47af9c1b3f35990e57cf424e9956abb
Linuxil <coder20022105@gmail.com>
2023-11-01 00:09:23 +0500

    added 3 new files

---
+++ index.js
@@ -0,0 +1,28 @@
+let number = 29
+let buttonga = "salom"
+
+//object
+let person = 
+{
+ name: 'Murodjon',
+ age: '20',
+ surname: 'Isroilov',
+ isMarried: false
+}
+console.log(person);
+
+//array
+let arr  = 
+[
+    15,
+    20,
+    25,
+    30,
+]
+console.log(arr)
+arr[1] = 100
+console.log(arr)
+let ans = prompt('What is your name:')
+console.log(ans)
+let answer = confirm('yoshingiz 18+ mi')
+console.log(answer);
